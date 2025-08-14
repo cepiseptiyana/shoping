84c28a1aa901ccec4c5290b2ed6dc2e765d2371e
cepiseptiyana <cepifams3@gmail.com>
2025-08-15 02:22:38 +0700

    feat: buat fitur login & register #dummy

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,13 @@ import { createBrowserRouter } from "react-router";
 
 // features
 import MainLayout from "./layouts/MainLayout.jsx";
+import AuthLayout from "./layouts/AuthLayout.jsx";
+
 import HomePage from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import DetailProduct from "./pages/Detail.jsx";
+
+import Login from "./features/login/Login.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +20,12 @@ const router = createBrowserRouter([
       { path: "products/:category", Component: Products },
       { path: "detail/:id", Component: DetailProduct },
     ],
+  },
+
+  {
+    path: "login",
+    Component: AuthLayout,
+    children: [{ index: true, Component: Login }],
   },
 ]);
 
